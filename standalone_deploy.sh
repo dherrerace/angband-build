4106926dcdd43504fc973b940b0c1ee30a4a9377
Diego Herrera <dherrerace@gmail.com>
2021-11-25 17:31:11 -0300

    Moved patches and out folders

--- standalone_deploy.sh
+++ standalone_deploy.sh
@@ -4,14 +4,17 @@ set -e
 
 podman build -t angband-standalone ./standalone
 
-podman unshare chown 0:0 -R $(pwd)/out
-rm -rf $(pwd)/out
-mkdir -p $(pwd)/out
-podman unshare chown $UID:$UID -R $(pwd)/out
+mkdir -p $(pwd)/standalone_out
+
+podman unshare chown 0:0 -R $(pwd)/standalone_out
+rm -rf $(pwd)/standalone_out
+mkdir -p $(pwd)/standalone_out
+podman unshare chown $UID:$UID -R $(pwd)/standalone_out
 
 podman run -ti --rm \
-    -v $(pwd)/out:/opt/angband:Z \
-    -v $(pwd)/angband:/opt/orig/:Z \
+    -v $(pwd)/standalone_out:/opt/angband:Z \
+    -v $(pwd)/angband:/opt/orig/:Z,ro \
+    -v $(pwd)/patch:/opt/patch/:Z,ro \
     localhost/angband-standalone:latest
 
-podman unshare chown 0:0 -R $(pwd)/out
+podman unshare chown 0:0 -R $(pwd)/standalone_out
